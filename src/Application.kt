494d031616f8786c6f0cca462719bc2c2bad6b7d
Jan Müller <janmueller3698@gmail.com>
2020-03-23 22:02:54 +0100

    Created project

---
+++ src/Application.kt
@@ -0,0 +1,60 @@
+package eu.yeger
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+import io.ktor.features.*
+import org.slf4j.event.*
+import io.ktor.routing.*
+import io.ktor.http.*
+import io.ktor.auth.*
+import com.fasterxml.jackson.databind.*
+import io.ktor.jackson.*
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+
+    install(CallLogging) {
+        level = Level.INFO
+        filter { call -> call.request.path().startsWith("/") }
+    }
+
+    install(DefaultHeaders) {
+        header("X-Engine", "Ktor") // will send this header with each response
+    }
+
+    install(Authentication) {
+    }
+
+    install(ContentNegotiation) {
+        jackson {
+            enable(SerializationFeature.INDENT_OUTPUT)
+        }
+    }
+
+    routing {
+        get("/") {
+            call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
+        }
+
+        install(StatusPages) {
+            exception<AuthenticationException> {
+                call.respond(HttpStatusCode.Unauthorized)
+            }
+            exception<AuthorizationException> {
+                call.respond(HttpStatusCode.Forbidden)
+            }
+
+        }
+
+        get("/json/jackson") {
+            call.respond(mapOf("hello" to "world"))
+        }
+    }
+}
+
+class AuthenticationException : RuntimeException()
+class AuthorizationException : RuntimeException()
---
+++ test/ApplicationTest.kt
@@ -0,0 +1,54 @@
+package eu.yeger
+
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.mock.MockEngine
+import io.ktor.client.engine.mock.respond
+import io.ktor.client.request.get
+import io.ktor.client.request.request
+import io.ktor.client.statement.HttpResponse
+import io.ktor.http.HttpMethod
+import io.ktor.http.HttpStatusCode
+import io.ktor.http.fullPath
+import io.ktor.http.headersOf
+import io.ktor.server.testing.handleRequest
+import io.ktor.server.testing.withTestApplication
+import io.ktor.utils.io.ByteReadChannel
+import kotlinx.coroutines.runBlocking
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ApplicationTest {
+
+    @Test
+    fun testRoot() {
+        withTestApplication({ module(testing = true) }) {
+            handleRequest(HttpMethod.Get, "/").apply {
+                assertEquals(HttpStatusCode.OK, response.status())
+                assertEquals("HELLO WORLD!", response.content)
+            }
+        }
+    }
+
+    @Test
+    fun testClientMock() {
+        runBlocking {
+            val client = HttpClient(MockEngine) {
+                engine {
+                    addHandler { request ->
+                        when (request.url.fullPath) {
+                            "/" -> respond(
+                                ByteReadChannel(byteArrayOf(1, 2, 3)),
+                                headers = headersOf("X-MyHeader", "MyValue")
+                            )
+                            else -> respond("Not Found ${request.url.encodedPath}", HttpStatusCode.NotFound)
+                        }
+                    }
+                }
+                expectSuccess = false
+            }
+            assertEquals(byteArrayOf(1, 2, 3).toList(), client.get<ByteArray>("/").toList())
+            assertEquals("MyValue", client.request<HttpResponse>("/") {}.headers["X-MyHeader"])
+            assertEquals("Not Found other/path", client.get("/other/path"))
+        }
+    }
+}
