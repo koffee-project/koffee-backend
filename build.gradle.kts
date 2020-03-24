27dd5baa575019e9f615043125c1c9ee82c4c392
Jan Müller <janmueller3698@gmail.com>
2020-03-24 11:07:59 +0100

    Migrated Gradle to Kotlin DSL

---
+++ build.gradle.kts
@@ -0,0 +1,58 @@
+val kotlin_version: String by project
+val ktor_version: String by project
+val logback_version: String by project
+
+plugins {
+    application
+    kotlin("jvm") version "1.3.71"
+    id("com.github.johnrengelman.shadow") version "5.2.0"
+}
+
+group = "eu.yeger"
+version = "0.0.1"
+
+application {
+    mainClassName = "io.ktor.server.netty.EngineMain"
+}
+
+sourceSets {
+    main {
+        java.srcDirs("src")
+        resources.srcDirs("resources")
+    }
+    test {
+        java.srcDirs("test")
+        resources.srcDirs("testresources")
+    }
+}
+
+repositories {
+    mavenLocal()
+    jcenter()
+    maven(url = "https://kotlin.bintray.com/ktor")
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlin_version")
+    implementation("io.ktor:ktor-server-netty:$ktor_version")
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+    implementation("io.ktor:ktor-server-core:$ktor_version")
+    implementation("io.ktor:ktor-server-host-common:$ktor_version")
+    implementation("io.ktor:ktor-auth:$ktor_version")
+    implementation("io.ktor:ktor-auth-jwt:$ktor_version")
+    implementation("io.ktor:ktor-jackson:$ktor_version")
+    implementation("io.ktor:ktor-client-core:$ktor_version")
+    implementation("io.ktor:ktor-client-core-jvm:$ktor_version")
+    implementation("io.ktor:ktor-client-cio:$ktor_version")
+    testImplementation("io.ktor:ktor-server-tests:$ktor_version")
+    testImplementation("io.ktor:ktor-client-mock:$ktor_version")
+    testImplementation("io.ktor:ktor-client-mock-jvm:$ktor_version")
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+tasks.withType<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar>  {
+    archiveBaseName.set("${project.name}-all")
+}
