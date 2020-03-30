f289252995a49fa0069948390362159e6071016c
Jan Müller <janmueller3698@gmail.com>
2020-03-30 22:59:52 +0200

    Fixed test

--- src/routing/UserRoutes.kt
+++ src/routing/UserRoutes.kt
@@ -1,13 +1,11 @@
 package eu.yeger.routing
 
 import eu.yeger.model.User
+import eu.yeger.model.respondWithResult
 import eu.yeger.service.UserService
-import eu.yeger.service.respondWithResult
 import io.ktor.application.call
-import io.ktor.http.ContentType
 import io.ktor.request.receive
 import io.ktor.response.respond
-import io.ktor.response.respondText
 import io.ktor.routing.Routing
 import io.ktor.routing.get
 import io.ktor.routing.post
@@ -15,10 +13,6 @@ import org.koin.ktor.ext.inject
 
 fun Routing.userRoutes() {
     val userService: UserService by inject()
-
-    get("/") {
-        call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
-    }
 
     get("/users") {
         val users = userService.getAllUsers()
