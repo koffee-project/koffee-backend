6ea177080d379ee11049340ce6e41efefbc05136
Jan Müller <janmueller3698@gmail.com>
2020-04-02 18:10:56 +0200

    Minor change to upsert extension

--- src/utility/KMongoUtilities.kt
+++ src/utility/KMongoUtilities.kt
@@ -4,9 +4,9 @@ import com.mongodb.client.model.UpdateOptions
 import eu.yeger.model.Entity
 import org.litote.kmongo.coroutine.CoroutineCollection
 
-suspend fun <T : Entity> CoroutineCollection<T>.upsert(entity: T) =
+suspend fun <T : Entity> CoroutineCollection<T>.upsert(entity: T, options: UpdateOptions = UpdateOptions()) =
     updateOneById(
         id = entity.id,
         update = entity,
-        options = UpdateOptions().upsert(true)
+        options = options.upsert(true)
     )
