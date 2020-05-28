c50e986cc456e68d2f5c00688148391d9ac84906
Jan Müller <janmueller3698@gmail.com>
2020-05-28 12:57:35 +0200

    IDs are now optional for User/Item creation
    Random UUIDs are used as fallback

--- src/model/domain/Item.kt
+++ src/model/domain/Item.kt
@@ -1,7 +1,9 @@
 package eu.yeger.model.domain
 
+import java.util.UUID
+
 data class Item(
-    override val id: String,
+    override val id: String = UUID.randomUUID().toString(),
     val name: String,
     val amount: Int?,
     val price: Double
--- src/model/dto/PartialUser.kt
+++ src/model/dto/PartialUser.kt
@@ -2,9 +2,10 @@ package eu.yeger.model.dto
 
 import eu.yeger.model.domain.TransactionList
 import eu.yeger.model.domain.User
+import java.util.UUID
 
 data class PartialUser(
-    val id: String,
+    val id: String = UUID.randomUUID().toString(),
     val name: String,
     val isAdmin: Boolean,
     val password: String?
