3787cb00b986c301d09c155911122b0aeb25ae42
Jan Müller <janmueller3698@gmail.com>
2020-07-02 12:48:13 +0200

    Documented repositories

--- src/repository/ItemRepository.kt
+++ src/repository/ItemRepository.kt
@@ -2,17 +2,55 @@ package eu.yeger.repository
 
 import eu.yeger.model.domain.Item
 
+/**
+ * Repository for [Item]s.
+ *
+ * @author Jan Müller
+ */
 interface ItemRepository {
 
+    /**
+     * Retrieves all [Item]s from the repository.
+     *
+     * @return All available [Item]s.
+     */
     suspend fun getAll(): List<Item>
 
+    /**
+     * Retrieves an [Item] from the repository.
+     *
+     * @param id The id of the [Item].
+     * @return The [Item] if available.
+     */
     suspend fun getById(id: String): Item?
 
+    /**
+     * Checks if the repository contains a specific [Item].
+     *
+     * @param id The id of the [Item].
+     * @return true if the repository contains the [Item].
+     */
     suspend fun hasItemWithId(id: String): Boolean
 
+    /**
+     * Inserts an [Item] into the repository.
+     *
+     * @param item The [Item] to be inserted.
+     */
     suspend fun insert(item: Item)
 
+    /**
+     * Removes an [Item] from the repository.
+     *
+     * @param id The id of the [Item] to be removed.
+     */
     suspend fun removeById(id: String)
 
+    /**
+     * Updates the amount of an [Item] from the repository.
+     *
+     * @param id The id of the [Item] to be updated.
+     * @param change The delta to be changed.
+     */
     suspend fun updateAmount(id: String, change: Int)
 }
